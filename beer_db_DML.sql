9e007743e8b7bb33a668ddbb4afc39bb435b808b
brian-sprague <39359899+brian-sprague@users.noreply.github.com>
2019-02-06 17:22:15 -0800

    Data manipulation for db (Still needs queries)

---
+++ beer_db_DML.sql
@@ -0,0 +1,67 @@
+/****************************************************************
+DATA MANIPULATION LAGUAGE FOR BEER DATABASE
+****************************************************************/
+
+/***BEER TABLE***/
+/*Insert a beer into the db*/
+
+
+/*Select a beer from the db*/
+
+
+/*Update a beer in the db*/
+
+
+/*Remove a beer from the db*/
+
+
+/***BREWERY TABLE***/
+/*Insert a brewery into the db*/
+
+
+/*Select a brewery from the db*/
+
+
+/*Update a brewery in the db*/
+
+
+/*Remove a brewery from the db*/
+
+
+/***STYLE TABLE***/
+/*Insert a style into the db*/
+
+
+/*Select a style from the db*/
+
+
+/*Update a style in the db*/
+
+
+/*Remove a style from the db*/
+
+
+/***VENUE TABLE***/
+/*Insert a venue into the db*/
+
+
+/*Select a venue from the db*/
+
+
+/*Update a venue in the db*/
+
+
+/*Remove a venue from the db*/
+
+
+/***REVIEW TABLE***/
+/*Insert a review into the db*/
+
+
+/*Select a review from the db*/
+
+
+/*Update a review in the db*/
+
+
+/*Remove a review from the db*/

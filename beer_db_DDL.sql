6b15fb84d45bc6ca558d85ceac4e822611b40015
Brian Sprague <spragubr@oregonstate.edu>
2019-02-09 12:13:05 -0800

    Checked in PHPmyAdmin
    All tables are created in the DB, just need to make sure FKs are handled correctly

--- beer_db_DDL.sql
+++ beer_db_DDL.sql
@@ -7,93 +7,75 @@ CREATE DATABASE beer_db;
 
 /*CREATE THE TABLES*/
 
-/*CREATE BEER TABLE*/
-CREATE TABLE beer (
-    id INTEGER primary key auto_increment,
-    name VARCHAR(100) NOT NULL,
-    brewery INTEGER UNSIGNED NOT NULL FOREIGN KEY,
-    style INTEGER UNSIGNED NOT NULL,
-    abv FLOAT,
-    ibu INTEGER,
-    PRIMARY KEY (id) 
-    CONSTRAINT 'fk_beer_brewery'
-        FOREIGN KEY (beer.brewery) REFERENCES brewery (id)
-        ON DELETE CASCADE                   /*Review specific needs for each entitiy*/
-        ON UPDATE RESTRICT                  /*Review specific needs for each entitiy*/
-    CONSTRAINT 'fk_beer_style'
-        FOREIGN KEY (beer.style) REFERENCES style (id)
-        ON DELETE CASCADE                   /*Review specific needs for each entitiy*/
-        ON UPDATE RESTRICT                  /*Review specific needs for each entitiy*/
-)
-
 /*CREATE BREWERY TABLE*/
 CREATE TABLE brewery (
-    id INTEGER primary key auto_increment,
+    id INTEGER AUTO_INCREMENT,
     name VARCHAR(100) NOT NULL,
     country VARCHAR(100) NOT NULL,
     city VARCHAR(100),
     state VARCHAR(100),                     /*May need to be VARCHAR(2) like state attribute in venue*/
     PRIMARY KEY (id) 
-)
+);
 
 /*CREATE STYLE TABLE*/
 CREATE TABLE style (
-    id INTEGER primary key auto_increment,
+    id INTEGER AUTO_INCREMENT,
     name VARCHAR(100) NOT NULL,
     description VARCHAR(300),
     abv_range VARCHAR(10),
     ibu_range VARCHAR(10),
     PRIMARY KEY (id) 
-)
+);
+
+/*CREATE BEER TABLE*/
+CREATE TABLE beer (
+    id INTEGER AUTO_INCREMENT,
+    name VARCHAR(100) NOT NULL,
+    brewery INTEGER NOT NULL,
+    style INTEGER NOT NULL,
+    abv FLOAT,
+    ibu INTEGER,
+    PRIMARY KEY (id),
+    FOREIGN KEY (brewery) REFERENCES brewery (id) ON DELETE CASCADE,                   /*Review specific needs for each entitiy*/
+    FOREIGN KEY (style) REFERENCES style (id) ON DELETE CASCADE                   /*Review specific needs for each entitiy*/
+);
 
 /*CREATE VENUE TABLE*/
 CREATE TABLE venue (
-    id INTEGER primary key auto_increment,
+    id INTEGER AUTO_INCREMENT,
     name VARCHAR(100) NOT NULL,
     address VARCHAR(100),
     city VARCHAR(100) NOT NULL,
-    state VARCHAR(2) NOT NULL,
+    state VARCHAR(100) NOT NULL,        /*Updated character count*/
     PRIMARY KEY (id) 
-)
+);
+
+/*CREATE USER TABLE*/
+CREATE TABLE db_user (
+    id INT AUTO_INCREMENT,
+    user_name VARCHAR(20) NOT NULL,
+    email VARCHAR(100),
+    PRIMARY KEY (id) 
+);
 
 /*CREATE REVIEW TABLE*/
 CREATE TABLE review (
-    id INT primary key auto_increment,
-    user_name VARCHAR(100),             /*Might need to change in Schema from "user*/
+    id INT AUTO_INCREMENT,
+    user_name INTEGER,             /*Might need to change in Schema from "user*/
     beer INTEGER,
-    rev_date DATE DEFAULT CURRENT_DATE, /*Might need to change in Schema from "date*, as date is a keyword*/
+    rev_date DATE, /*Might need to change in Schema from "date*, as date is a keyword*/
     rating INTEGER NOT NULL,            /*Should be NOT NULL in schema*/
     comments VARCHAR(2),
-    PRIMARY KEY (id)
-    CONSTRAINT 'fk_review_beer'
-        FOREIGN KEY (beer) REFERENCES beer (id)
-        ON DELETE CASCADE                   /*Review specific needs for each entitiy*/
-        ON UPDATE RESTRICT                  /*Review specific needs for each entitiy*/
-    CONSTRAINT 'fk_review_user'
-        FOREIGN KEY (user) REFERENCES user (id)
-        ON DELETE CASCADE                   /*Review specific needs for each entitiy*/
-        ON UPDATE RESTRICT                  /*Review specific needs for each entitiy*/
-)
-
-/*CREATE USER TABLE*/
-CREATE TABLE user (
-    id INT primary key auto_increment,
-    username VARCHAR(20) NOT NULL,
-    email VARCHAR(100),
-    PRIMARY KEY (id) 
-)
+    PRIMARY KEY (id),
+    FOREIGN KEY (beer) REFERENCES beer (id) ON DELETE CASCADE, /*Review specific needs for each entitiy*/
+    FOREIGN KEY (user_name) REFERENCES db_user (id) ON DELETE CASCADE /*Review specific needs for each entitiy*/
+);
 
 /*CREATE BEER_VENUE TABLE*/
 CREATE TABLE beer_venue (
     beer INTEGER,
     venue INTEGER, 
-    PRIMARY KEY (beer, venue)
-    CONSTRAINT 'fk_beer_venue_beer'
-        FOREIGN KEY (beer) REFERENCES beer (id)
-        ON DELETE CASCADE                   /*Review specific needs for each entitiy*/
-        ON UPDATE RESTRICT                  /*Review specific needs for each entitiy*/
-    CONSTRAINT 'fk_beer_venue_venue'
-        FOREIGN KEY (venue) REFERENCES venue (id)
-        ON DELETE CASCADE                   /*Review specific needs for each entitiy*/
-        ON UPDATE RESTRICT                  /*Review specific needs for each entitiy*/
-)
+    PRIMARY KEY (beer, venue),
+    FOREIGN KEY (beer) REFERENCES beer (id) ON DELETE CASCADE, /*Review specific needs for each entitiy*/
+    FOREIGN KEY (venue) REFERENCES venue (id) ON DELETE CASCADE /*Review specific needs for each entitiy*/
+);

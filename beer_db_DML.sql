3c8c7aa0859e365f54bb0396df3fb74751c2bd5e
Brian Sprague <spragubr@oregonstate.edu>
2019-02-10 10:11:29 -0800

    Added queries (still needs testing)

--- beer_db_DML.sql
+++ beer_db_DML.sql
@@ -2,66 +2,65 @@
 DATA MANIPULATION LAGUAGE FOR BEER DATABASE
 ****************************************************************/
 
-/***BEER TABLE***/
-/*Insert a beer into the db*/
+--Add a beer
+INSERT INTO beer (name, brewery, style, abv, ibu)
+VALUES (:nameInput, :breweryInput, :styleInput, :abvInput, :ibuInput)
 
+--Add a brewery
+INSERT INTO brewery (name, country, state, city)
+VALUES (:nameInput, :countryInput, :stateInput, :cityInput)
 
-/*Select a beer from the db*/
+--Add a style
+INSERT INTO style (name, description, abv_range, ibu_range)
+VALUES (:nameInput, :descriptionInput, :abv_rangeInput, :ibu_rangeInput)
 
+--Add a venue
+INSERT INTO venue (name, address, city, state)
+VALUES (:nameInput, :addressInput, :cityInput, :stateInput)
 
-/*Update a beer in the db*/
+--Add a review
+INSERT INTO review (user_name, beer, rev_date, rating, comments)
+VALUES (:user_nameInput, :beerInput, :rev_dateInput, :ratingInput, :commentsInput)
 
+--Add a beer to  a venue's tap list
+INSERT INTO beer_venue (beer, venue)
+VALUES (:beerInput, :venueInput)
 
-/*Remove a beer from the db*/
+--Select all beers from the db
+SELECT id, name, brwery, style, abv, ibu FROM beer 
 
+--Select all beers for a single brewery
+SELECT id, name, brewery, style FROM beer WHERE brewery = :breweryInput
 
-/***BREWERY TABLE***/
-/*Insert a brewery into the db*/
+--Select all beers for a single style
+SELECT id, name, brewery, style FROM beer WHERE style = :styleInput
 
+--Select all beers for a single venue
+SELECT id, name, brewery, style FROM beer INNER JOIN beer_venue ON beer.id=beer_venue.beer WHERE beer_venue.venue=:venueInput
 
-/*Select a brewery from the db*/
+--Select all venues for venue page
+SELECT id, name, city, state FROM venue
 
+--Select a single venue
+SELECT id, name, address, city, state FROM venue 
 
-/*Update a brewery in the db*/
+--Select all reviews for a beer
+SELECT id, user_name, rev_date, rating, comments FROM review INNER JOIN beer ON review.beer=beer.id WHERE review.beer=:beerInput
 
+--Update a beer in the db
+UPDATE beer SET name=:nameInput, 
+                brewery=:breweryInput, 
+                style=:styleInput, 
+                abv=:abvInput, 
+                ibu=ibuInput
+    WHERE id=:idInput
 
-/*Remove a brewery from the db*/
+--Update a brewery
+UPDATE brewery SET name=:nameInput, 
+                country=:countryInput, 
+                city=:cityInput, 
+                state=:stateInput, 
+    WHERE id=:idInput
 
-
-/***STYLE TABLE***/
-/*Insert a style into the db*/
-
-
-/*Select a style from the db*/
-
-
-/*Update a style in the db*/
-
-
-/*Remove a style from the db*/
-
-
-/***VENUE TABLE***/
-/*Insert a venue into the db*/
-
-
-/*Select a venue from the db*/
-
-
-/*Update a venue in the db*/
-
-
-/*Remove a venue from the db*/
-
-
-/***REVIEW TABLE***/
-/*Insert a review into the db*/
-
-
-/*Select a review from the db*/
-
-
-/*Update a review in the db*/
-
-
-/*Remove a review from the db*/
+--Remove a beer from the db
+DELETE FROM beer WHERE id=:idInpupt

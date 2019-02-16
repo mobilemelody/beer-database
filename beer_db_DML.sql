640d81e54dd9440858fc1bcc623dbfe57d7a0463
mobilemelody <melody.reebs@gmail.com>
2019-02-16 09:55:00 -0800

    Added DML queries and updated display tables

--- beer_db_DML.sql
+++ beer_db_DML.sql
@@ -2,82 +2,166 @@
 DATA MANIPULATION LAGUAGE FOR BEER DATABASE
 ****************************************************************/
 
---Add a beer
+/* ADD QUERIES */
+-- Add a beer
 INSERT INTO beer (name, brewery, style, abv, ibu)
 VALUES (:nameInput, :breweryInput, :styleInput, :abvInput, :ibuInput)
 
---Add a brewery
+-- Add a brewery
 INSERT INTO brewery (name, country, state, city)
 VALUES (:nameInput, :countryInput, :stateInput, :cityInput)
 
---Add a style
+-- Add a style
 INSERT INTO style (name, description, abv_range, ibu_range)
 VALUES (:nameInput, :descriptionInput, :abv_rangeInput, :ibu_rangeInput)
 
---Add a venue
+-- Add a venue
 INSERT INTO venue (name, address, city, state)
 VALUES (:nameInput, :addressInput, :cityInput, :stateInput)
 
---Add a review
+-- Add a review
 INSERT INTO review (user_name, beer, rev_date, rating, comments)
 VALUES (:user_nameInput, :beerInput, :rev_dateInput, :ratingInput, :commentsInput)
 
---Add a user
+-- Add a user
 INSERT INTO db_user (user_name, email)
 VALUES (:user_nameInput, :emailInput)
 
---Add a beer to  a venue's tap list
+-- Add a beer to  a venue's tap list
 INSERT INTO beer_venue (beer, venue)
 VALUES (:beerInput, :venueInput)
 
---Search for beers matching criteria
-SELECT id, beer.name, brewery.name, style.name, beer.abv, beer.ibu AVG(review.rating) FROM beer 
+/* SEARCH PAGE */
+-- Search for beers matching criteria
+SELECT beer.id, beer.name, brewery.id, brewery.name, style.id, style.name, AVG(review.rating) AS avg_rating
+FROM beer 
 INNER JOIN brewery ON beer.brewery=brewery.id
 INNER JOIN style ON beer.style=style.id 
 LEFT JOIN review ON beer.id=review.beer
-WHERE name LIKE '%'+:searchInput+'%' 
-    OR brewery.name LIKE '%'+:searchInput+'%' 
-    OR style.name LIKE '%'+:searchInput+'%'
+WHERE beer.name LIKE CONCAT('%', :searchInput, '%') 
+    OR brewery.name LIKE CONCAT('%', :searchInput, '%') 
+    OR style.name LIKE CONCAT('%', :searchInput, '%') 
 GROUP BY beer.id 
 
---Show beers in descending order based on average rating
-SELECT beer.id, beer.name, brewery.name, style.name, beer.abv, beer.ibu, AVG(review.rating) FROM beer
-INNER JOIN  brewery ON beer.brewery=brewery.id
-INNER JOIN  style ON beer.style=style.id
+/* BEER PAGES */
+-- Show beers in descending order based on average rating
+SELECT beer.id, beer.name, brewery.id, brewery.name, style.id, style.name, AVG(review.rating) AS avg_rating, COUNT(review.rating) AS num_reviews
+FROM beer
+INNER JOIN brewery ON beer.brewery=brewery.id
+INNER JOIN style ON beer.style=style.id
 LEFT JOIN review ON beer.id=review.beer
 GROUP BY beer.id 
-ORDER BY review.rating DESC
+ORDER BY avg_rating DESC
 
---Select all breweries
-SELECT id, name, country, state, city FROM brewery 
+-- Select individual beer
+SELECT * FROM beer WHERE id=:idInput
 
---Select all styles, list alphabetically and show number of beers per style
-SELECT style.id, style.name, COUNT(beer.id) FROM style 
-INNER JOIN beer ON style.id=beer.style
+-- Select reviews for individual beer
+SELECT * FROM review WHERE beer=:beerInput
+
+-- Select all users sorted alphabetically for dropdown in reviews form
+SELECT id, user_name FROM db_user ORDER BY user_name ASC
+
+-- Select all breweries sorted alphabetically for dropdown in beer form
+SELECT id, name FROM brewery ORDER BY name ASC
+
+-- Select all styles sorted alphabetically for dropdown in beer form
+SELECT id, name FROM style ORDER BY name ASC
+
+/* STYLE PAGES */
+-- Select all styles, list alphabetically and show number of beers per style
+SELECT style.id, style.name, COUNT(beer.id)
+FROM style 
+LEFT JOIN beer ON style.id=beer.style
 GROUP BY style.id
 ORDER BY style.name ASC
 
---Select all beers for a single brewery
-SELECT id, name, brewery, style FROM beer WHERE brewery = :breweryInput
+-- Select individual style
+SELECT * FROM style WHERE id=:idInput
 
---Select all beers for a single style
-SELECT id, name, brewery, style FROM beer WHERE style = :styleInput
+-- Select all beers for a single style sorted by average rating
+SELECT beer.id, beer.name, brewery.id, brewery.name, AVG(review.rating) AS avg_rating, COUNT(review.rating) AS num_reviews
+FROM beer
+INNER JOIN brewery ON beer.brewery=brewery.id
+LEFT JOIN review ON beer.id=review.beer
+WHERE beer.style=:styleInput
+GROUP BY beer.id
+ORDER BY avg_rating DESC
 
---Select all beers for a single venue
-SELECT id, name, brewery, style FROM beer 
-INNER JOIN beer_venue ON beer.id=beer_venue.beer WHERE beer_venue.venue=:venueInput
+/* BREWERY PAGES */
+-- Select all breweries sorted by average beer rating
+SELECT brewery.id, brewery.name, AVG(review.rating) AS avg_rating, COUNT(beer.id) AS num_reviews
+FROM brewery
+LEFT JOIN beer ON brewery.id=beer.brewery
+LEFT JOIN review ON beer.id=review.beer
+GROUP BY brewery.id
+ORDER BY avg_rating DESC
 
---Select all venues for venue page
-SELECT id, name, city, state FROM venue
+-- Select individual brewery
+SELECT * FROM brewery WHERE id=:idInput
 
---Select a single venue
-SELECT id, name, address, city, state FROM venue 
+-- Select all beers for a single brewery sorted by average rating
+SELECT beer.id, beer.name, style.id, style.name, AVG(review.rating) AS avg_rating, COUNT(review.rating) AS num_reviews
+FROM beer
+INNER JOIN style ON beer.style=style.id
+LEFT JOIN review ON beer.id=review.beer
+WHERE beer.brewery=:breweryInput
+GROUP BY beer.id
+ORDER BY avg_rating DESC
 
---Select all reviews for a beer
-SELECT id, user_name, rev_date, rating, comments FROM review 
-INNER JOIN beer ON review.beer=beer.id WHERE review.beer=:beerInput
+-- Select all countries sorted alphabetically for dropdown in brewery form
+SELECT * FROM country
 
---Update a beer
+/* VENUE PAGES */
+-- Select all cities and states
+SELECT DISTINCT state, city FROM venue ORDER BY state, city
+
+-- Select all venues for single state
+SELECT venue.id, venue.name, venue.city, COUNT(beer_venue.beer) AS num_beers
+FROM venue
+LEFT JOIN beer_venue ON venue.id=beer_venue.venue
+WHERE venue.state=:stateInput
+GROUP BY venue.id
+ORDER BY venue.name ASC
+
+-- Select all venues for single city
+SELECT venue.id, venue.name, venue.address, COUNT(beer_venue.beer) AS num_beers
+FROM venue
+LEFT JOIN beer_venue ON venue.id=beer_venue.venue
+WHERE venue.city=:cityInput
+GROUP BY venue.id
+ORDER BY venue.name ASC
+
+-- Select a single venue
+SELECT * FROM venue 
+
+-- Select all beers for a single venue sorted alphabetically
+SELECT beer.id, beer.name, brewery.id, brewery.name, style.id, style.name, AVG(review.rating) AS avg_rating, COUNT(review.rating) AS num_reviews
+FROM beer 
+INNER JOIN beer_venue ON beer.id=beer_venue.beer
+INNER JOIN brewery ON beer.brewery=brewery.id
+INNER JOIN style ON beer.style=style.id 
+LEFT JOIN review ON beer.id=review.beer
+WHERE beer_venue.venue=:venueInput
+GROUP BY beer.id
+ORDER BY beer.name ASC
+
+/* USER PAGES */
+-- Select all users sorted by number of reviews
+SELECT db_user.id, db_user.user_name, COUNT(review.id) AS num_reviews
+FROM db_user
+LEFT JOIN review ON db_user.id=review.user_name
+GROUP BY db_user.id
+ORDER BY num_reviews DESC
+
+-- Select all reviews by user sorted by most recent
+SELECT *
+FROM review
+WHERE user_name=:userInput
+ORDER BY rev_date DESC
+
+/* UPDATE QUERIES */
+-- Update a beer
 UPDATE beer SET name=:nameInput, 
                 brewery=:breweryInput, 
                 style=:styleInput, 
@@ -85,7 +169,7 @@ UPDATE beer SET name=:nameInput,
                 ibu=ibuInput
     WHERE id=:idInput
 
---Update a brewery
+-- Update a brewery
 UPDATE brewery SET name=:nameInput, 
                 description=:descriptionInput, 
                 country=countryInput,
@@ -93,47 +177,48 @@ UPDATE brewery SET name=:nameInput,
                 state=:stateInput
     WHERE id=:idInput
 
---Update a style
+-- Update a style
 UPDATE style SET name=:nameInput, 
                 address=:addressInput, 
                 abv_range=:abv_rangeInput, 
                 ibu_range=:ibu_rangeInput 
     WHERE id=:idInput
 
---Update a venue
+-- Update a venue
 UPDATE venue SET name=:nameInput, 
                 address=:addressInput, 
                 city=:cityInput, 
                 state=:stateInput
     WHERE id=:idInput
 
---Update a review's rating, comments, and date
+-- Update a review's rating, comments, and date
 UPDATE review SET rev_date=:rev_dateInput, 
                 rating=:ratingInput, 
                 comments=:commentsInput  
     WHERE id=:idInput
 
---Update a user's email address
+-- Update a user's email address
 UPDATE db_user SET email=:emailInput 
     WHERE id=:idInput
 
---Remove a beer from the db
+/* DELETE QUERIES */
+-- Remove a beer from the db
 DELETE FROM beer WHERE id=:idInpupt
 
---Remove a style from the db
+-- Remove a style from the db
 DELETE FROM style WHERE id=:idInpupt
 
---Remove a brewery from the db
+-- Remove a brewery from the db
 DELETE FROM brewery WHERE id=:idInpupt
 
---Remove a venue from the db
+-- Remove a venue from the db
 DELETE FROM venue WHERE id=:idInpupt
 
---Remove a review from the dp
+-- Remove a review from the dp
 DELETE FROM review WHERE id=:idInpupt
 
---Remove a user from the db
+-- Remove a user from the db
 DELETE FROM db_user WHERE id=:idInpupt
 
---Remove a beer from a venue
+-- Remove a beer from a venue
 DELETE FROM beer_venue WHERE id=:idInpupt

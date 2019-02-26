4ee45625bb5a57cd287835a4524f7444e0e0c645
mobilemelody <melody.reebs@gmail.com>
2019-02-25 19:22:24 -0800

    DB configuration

--- dbcon.js
+++ dbcon.js
@@ -1,9 +1,9 @@
 const mysql = require('mysql');
 const pool = mysql.createPool({
   connectionLimit : 10,
-  host            : DB_HOST,
-  user            : DB_USER,
-  password        : DB_PASSWORD,
-  database        : DB_NAME
+  host            : process.env.DB_HOST,
+  user            : process.env.DB_USER,
+  password        : process.env.DB_PASSWORD,
+  database        : process.env.DB_NAME
 });
 module.exports.pool = pool;

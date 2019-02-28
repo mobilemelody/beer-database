14a13227f5a821adb726f93bcc39bd1179244327
Brian Sprague <spragubr@oregonstate.edu>
2019-02-27 21:21:44 -0800

    update to gitignore

--- dbcon.js
+++ dbcon.js
@@ -1,3 +1,9 @@
 const mysql = require('mysql');
-const pool = mysql.createPool(process.env.DB_URL);
+const pool = mysql.createPool({
+    connectionLimit :   10,
+    host            :   process.env.DB_HOST,
+    user            :   process.env.DB_USER,
+    password        :   process.env.DB_PW,
+    database        :   process.env.DB_DB
+});
 module.exports.pool = pool;

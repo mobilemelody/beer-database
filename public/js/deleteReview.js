6e14083e0e95c73120d732bc442766e8ea7196f3
Brian Sprague <spragubr@oregonstate.edu>
2019-03-04 19:50:00 -0800

    Changed deleteReview location

---
+++ public/js/deleteReview.js
@@ -0,0 +1,13 @@
+function deleteReview(id){
+    $.ajax({
+        url: '/reviews/' + id,
+        type: 'DELETE',
+        success: function(result){
+            if(result.responseText != undefined) {
+                alert(result.responseText);
+            } else {
+                window.location.reload(true);
+            }
+        }
+    })
+};
